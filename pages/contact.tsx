2cb3da392323512afea2681fb23d6d73d1de19d2
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-11-19 13:28:19 +0100

    added project and contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Contact: NextPage = () => {
+  return <h1>contact page</h1>;
+};
+
+export default Contact;
---
+++ pages/projects.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Projects: NextPage = () => {
+  return <h1>Projects page</h1>;
+};
+
+export default Projects;
