18e90d6196e7450e886e4f4e41354130d27ca99d
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-11-21 17:01:20 +0100

    added react-axe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import React from "react";
 import { Layout } from "../components/Layout";
+
+if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") {
+  const ReactDOM = require("react-dom");
+  const axe = require("@axe-core/react");
+  axe(React, ReactDOM, 1000);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
